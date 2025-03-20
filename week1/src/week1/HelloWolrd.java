42558452a3688e57289edcbcbcdb6a52f076983b
JaeSunEo <wotjs020708@gmail.com>
2025-03-20 10:36:28 +0900

    add week1

---
+++ week1/src/week1/HelloWolrd.java
@@ -0,0 +1,11 @@
+package week1;
+
+public class HelloWolrd {
+	public static void main(String[] args) {
+		
+		System.out.println("학과: 컴퓨터정보학");
+		System.out.println("학번: 202144019");
+		System.out.println("이름: 어재선");
+	
+	}
+}
