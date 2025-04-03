3b1a349ff799e14f3b7586eda2f5b08531ffd46d
JaeSunEo <wotjs020708@gmail.com>
2025-04-03 11:54:34 +0900

    add ForNestedEx1

---
+++ week5/src/week5/ForNestedEx1.java
@@ -0,0 +1,16 @@
+package week5;
+
+public class ForNestedEx1 {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i <= 3; i++) {
+			for (int j=1; j<= 5; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+
+	}
+
+}
