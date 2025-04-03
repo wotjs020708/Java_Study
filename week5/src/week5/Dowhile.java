2a957d1fdcac514c0a3fa83ef6b736aaf55ba3a1
JaeSunEo <wotjs020708@gmail.com>
2025-04-03 11:27:58 +0900

    add Dowhile

---
+++ week5/src/week5/Dowhile.java
@@ -0,0 +1,23 @@
+package week5;
+
+import java.util.Scanner;
+
+public class Dowhile {
+
+	public static void main(String[] args) {
+		System.out.println("메세지를 입력하세요");
+		System.out.println("프로그램을 종료하려면 q를 입력하세요");
+		
+		Scanner sc = new Scanner(System.in);
+		
+		String inStr;
+		do {
+			System.out.print(">> ");
+			inStr = sc.nextLine();
+			System.out.println(inStr);
+			} while(!inStr.equals("q"));			
+		
+		System.out.println("프로그 종료");
+		sc.close();
+	}
+}
