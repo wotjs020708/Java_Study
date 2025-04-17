fdf40a370d0bd26a954e7884cad21977f2343160
JaeSunEo <wotjs020708@gmail.com>
2025-04-17 11:27:50 +0900

    add Korean, KoreanEx

---
+++ week7/src/week7/Korean.java
@@ -0,0 +1,18 @@
+package week7;
+
+public class Korean {
+	// 필드 선언
+	String nation = "대한민국";
+	String name;
+	String ssn;
+	String phone;
+	
+	// 객체 생성 시 매개변수로 초기화
+	// 생성자 정의
+	Korean(String name, String ssn, String phone) {
+		this.name  = name;
+		this.ssn = ssn;
+		this.phone = phone;
+		
+	}
+}
---
+++ week7/src/week7/KoreanEx.java
@@ -0,0 +1,19 @@
+package week7;
+
+public class KoreanEx {
+
+	public static void main(String[] args) {
+		// 실행 클래스
+		// 객체 생성
+		Korean k1 = new Korean("홍길동", "96062412301234", "010-1234-1234");
+		System.out.println("k1.nation = " + k1.nation);
+		System.out.println("k1.name = " + k1.name);
+		System.out.println("k1.ssn = " + k1.ssn);
+		System.out.println("k1.phone" + k1.phone);
+		
+		Korean k2 = new Korean("어선장", "02070812301234", "010-2314-2314");
+		
+
+	}
+
+}
