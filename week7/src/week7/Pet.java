7a0dfa7d86dc09c228d3bae6ec395159c5d3ac0b
JaeSunEo <wotjs020708@gmail.com>
2025-04-17 12:07:48 +0900

    uypdate Pet, PetEx

--- week7/src/week7/Pet.java
+++ week7/src/week7/Pet.java
@@ -5,11 +5,27 @@ public class Pet {
 	String name;
 	String type;
 	int age;
+	String color;
 	
+	Pet(String name, String type){
+		
+		this.name = name;
+		this.type = type;
+		this.age = 0;
+		this.color = "미정";
+		
+	}
 	Pet(String name, String type, int age){
 		this.name = name;
 		this.type = type;
 		this.age = age;
+		
+	}
+	Pet(String name, String type, int age, String color){
+		this.name = name;
+		this.type = type;
+		this.age = age;
+		this.color = color;
 		
 	}
 }
--- week7/src/week7/petEx.java
+++ week7/src/week7/petEx.java
@@ -6,7 +6,14 @@ public class petEx {
 		Pet pet1 = new Pet("바둑이", "강아지", 3);
 		
 		System.out.printf("이름: %s \n종류: %s \n나이: %d살\n", pet1.name, pet1.type, pet1.age);
+		
+		
+		Pet pet2 = new Pet("초코","고양이");
+		System.out.printf("이름: %s \n종류: %s \n나이: %d살\n색상: %s\n", pet2.name, pet2.type, pet2.age,pet2.color);
+		Pet pet3 = new Pet("구름이", "강아지", 2, "흰색");
+		System.out.printf("이름: %s \n종류: %s \n나이: %d살\n색상: %s\n", pet3.name, pet3.type, pet3.age,pet3.color);
 
+				
 	}
 
 }
