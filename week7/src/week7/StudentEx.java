e6e6eecbad215efc87b05433ae4b59d0772f2363
JaeSunEo <wotjs020708@gmail.com>
2025-04-17 11:06:01 +0900

    add Student, StudentEx

---
+++ week7/src/week7/StudentEx.java
@@ -0,0 +1,18 @@
+package week7;
+
+public class StudentEx {
+
+	public static void main(String[] args) {
+		// 실행할 목적으로 만드는 실행 클래
+		// 라이브러리용 클래스를 이용해서 객체 생성
+		
+		// st1 Student의 인스턴스이다.
+		Student st1 = new Student();
+		System.out.println("str1 변수가 Studen 객체를 참조한다.");
+		
+
+		Student st2 = new Student();
+		System.out.println("str2 변수가 또 다른 Studen 객체를 참조한다.");
+	}
+
+}
