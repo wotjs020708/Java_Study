2e0e3519e5c79f344f42557fd5444ee266b1343f
JaeSunEo <wotjs020708@gmail.com>
2025-05-01 22:37:42 +0900

    add Area

---
+++ week9/src/week9/Area.java
@@ -0,0 +1,14 @@
+package week9;
+
+public class Area {
+	double areaCal(double radius) {
+		return 3.14*radius*radius;
+	}
+	int areaCal(int width) {
+		return width*width;
+	}
+	int areaCal(int width, int height) {
+		return width*height;
+	}
+	
+}
