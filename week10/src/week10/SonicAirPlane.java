1675abef91c3c5e1c8103fc87f74c1f942f7f4d7
JaeSunEo <wotjs020708@gmail.com>
2025-05-08 11:59:54 +0900

    Ovrride

--- week10/src/week10/SonicAirPlane.java
+++ week10/src/week10/SonicAirPlane.java
@@ -3,6 +3,14 @@ package week10;
 public class SonicAirPlane extends AirPlane {
 	private int soundSpeed;
 	private int maxSpeed;
+	
+	//부모 메소드를 재정
+	@Override
+	public void fly() {
+		System.out.println("음속비행합니다.");
+	}
+	
+	
 	public int getSoundSpeed() {
 		return soundSpeed;
 	}
